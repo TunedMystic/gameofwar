9d07585e71699a8d06d129367c2e9563d560e7ad
sangeeta <sjbitcode@gmail.com>
2015-03-26 03:48:41 +0000

    add is_valid for knight piece

--- chesspiece.py
+++ chesspiece.py
@@ -36,3 +36,18 @@ class RookMixin(object):
 
 class Rook(RookMixin, ChessPiece):
   name = "Rook"
+
+
+class Knight(ChessPiece):
+  
+  def is_valid(self, row, col):
+    if self.current_position == (row, col):
+      return False
+    if (self.current_position[0] + 2) == row or (self.current_position[0] - 2) == row:
+      if (self.current_position[1] + 1) == col or (self.current_position[1] - 1) == col:
+        return True
+    if (self.current_position[1] + 2) == col or (self.current_position[1] - 2) == col:
+      if (self.current_position[0] + 1) == row or (self.current_position[0] - 1) == row:
+        return True
+    else:
+      return False
